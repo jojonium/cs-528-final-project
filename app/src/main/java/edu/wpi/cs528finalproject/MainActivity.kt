5a0dfab43a4a1395f0b2ed90cd2ce19055394f25
lokesh234 <lgangaramaney@wpi.edu>
2020-11-25 16:17:37 -0500

    Completed Login and Signup forms and the bottom navigation

--- app/src/main/java/edu/wpi/cs528finalproject/MainActivity.kt
+++ app/src/main/java/edu/wpi/cs528finalproject/MainActivity.kt
@@ -2,10 +2,17 @@ package edu.wpi.cs528finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_web)
+        val webView = findViewById<WebView>(R.id.webview)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://www.cdc.gov/coronavirus/2019-ncov/index.html")
+        val webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
     }
 }
