cc65d88dff5e361f796a640b920a6ddd07588bee
lokesh234 <lgangaramaney@wpi.edu>
2020-12-01 14:23:58 -0500

    Signup completed, Login still needs work

--- app/src/main/java/edu/wpi/cs528finalproject/MainActivity.kt
+++ app/src/main/java/edu/wpi/cs528finalproject/MainActivity.kt
@@ -2,17 +2,47 @@ package edu.wpi.cs528finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_web)
-        val webView = findViewById<WebView>(R.id.webview)
-        webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://www.cdc.gov/coronavirus/2019-ncov/index.html")
-        val webSettings = webView.settings
-        webSettings.javaScriptEnabled = true
+        setContentView(R.layout.activity_signup)
+//        val webView = findViewById<WebView>(R.id.webview)
+//        webView.webViewClient = WebViewClient()
+//        webView.loadUrl("https://www.cdc.gov/coronavirus/2019-ncov/index.html")
+//        val webSettings = webView.settings
+//        webSettings.javaScriptEnabled = true
+
+        val signupbutton = findViewById<Button>(R.id.button2);
+
+        signupbutton.setOnClickListener {
+            RegisterUser()
+        }
     }
+
+    private fun RegisterUser() {
+        val useremail = findViewById<EditText>(R.id.emailinput).text.toString();
+        val userpassword = findViewById<EditText>(R.id.passwordinput).text.toString();
+
+        if (useremail.isEmpty() || userpassword.isEmpty()) {
+            Toast.makeText(this, "Email or Password is Empty !", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(useremail, userpassword).addOnCompleteListener {
+            if(!it.isSuccessful) return@addOnCompleteListener
+
+            Log.d("Main", "Successfully created with uid: ${it.result?.user?.uid}")
+        }.addOnFailureListener {
+            Toast.makeText(this, "Could not sign up : ${it.message}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
