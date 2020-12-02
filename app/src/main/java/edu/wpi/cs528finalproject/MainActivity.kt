a32810b36f96386c3323418f8c62e749aa8abc21
lokesh234 <lgangaramaney@wpi.edu>
2020-12-01 20:28:27 -0500

    Completed adding information about the user to firebase

--- app/src/main/java/edu/wpi/cs528finalproject/MainActivity.kt
+++ app/src/main/java/edu/wpi/cs528finalproject/MainActivity.kt
@@ -11,11 +11,18 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        database = Firebase.database.reference
 //        val webView = findViewById<WebView>(R.id.webview)
 //        webView.webViewClient = WebViewClient()
 //        webView.loadUrl("https://www.cdc.gov/coronavirus/2019-ncov/index.html")
@@ -37,16 +44,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun RegisterUser() {
         val useremail = findViewById<EditText>(R.id.emailinput).text.toString();
+        val userfullname = findViewById<EditText>(R.id.fullnameinput).text.toString();
         val userpassword = findViewById<EditText>(R.id.passwordinput).text.toString();
 
-        if (useremail.isEmpty() || userpassword.isEmpty()) {
-            Toast.makeText(this, "Email or Password is Empty !", Toast.LENGTH_SHORT).show()
+        if (useremail.isEmpty() || userpassword.isEmpty() || userfullname.isEmpty()) {
+            Toast.makeText(this, "One of the above fields is empty !", Toast.LENGTH_SHORT).show()
             return
         }
 
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(useremail, userpassword).addOnCompleteListener {
             if(!it.isSuccessful) return@addOnCompleteListener
 
+            database.child("users").child(it.result?.user?.uid.toString()).child("username").setValue(userfullname)
+            database.child("users").child(it.result?.user?.uid.toString()).child("useremail").setValue(useremail)
             Log.d("Main", "Successfully created with uid: ${it.result?.user?.uid}")
         }.addOnFailureListener {
             Toast.makeText(this, "Could not sign up : ${it.message}", Toast.LENGTH_SHORT).show()
